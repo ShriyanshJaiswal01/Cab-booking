af7384a9635eb63e17894cacf9db2a40562f6b85
ShriyanshJaiswal01 <shriyanshmgs235@gmail.com>
2026-02-05 13:05:39 +0530

    Fix CORS, cookies, axios and socket.io for production

--- BACKEND/db/db.js
+++ BACKEND/db/db.js
@@ -1,30 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-function connectToDb(){
-  mongoose.connect(process.env.DB_CONNECT).then(()=>{
-        const mongoose = require('mongoose');
-    
-    function connectToDb() {
-      const dbUri = process.env.DB_CONNECT;
-    
-      if (!dbUri) {
-        console.error('DB_CONNECT environment variable is not set');
-        process.exit(1);
-      }
-    
-      mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => {
-          console.log('Connected to DB');
-        })
-        .catch(err => {
-          console.error('Failed to connect to DB', err);
-          process.exit(1);
-        });
-    }
-    
-    module.exports = connectToDb;
-    console.log('Connected to DB');
-  }).catch(err => console.log(err));
+function connectToDb() {
+  const dbUri = process.env.DB_CONNECT;
+
+  if (!dbUri) {
+    console.error('DB_CONNECT environment variable is not set');
+    process.exit(1);
+  }
+
+  mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log('Connected to DB');
+    })
+    .catch(err => {
+      console.error('Failed to connect to DB', err);
+      process.exit(1);
+    });
 }
 
 module.exports = connectToDb;
